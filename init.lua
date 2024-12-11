dd70713a307a5aff6a32e6be6fd428d7ecce0a1a
Denzel Wendy <d.wendy@verihubs.com>
2024-12-11 22:56:20 +0700

    init

---
+++ init.lua
@@ -0,0 +1,4 @@
+require("config.lazy")
+
+vim.opt.shiftwidth = 4
+vim.opt.number = True
---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'ellisonleao/gruvbox.nvim',
+    enabled = true,
+    config = function()
+      vim.cmd('colorscheme gruvbox')
+    end
+  }
+}
