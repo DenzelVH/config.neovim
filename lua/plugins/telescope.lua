7256650303abf3ee70e3a5f85b3ba9efc380439d
Denzel Wendy <d.wendy@verihubs.com>
2024-12-12 18:21:31 +0700

    update README and keybind

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -43,7 +43,7 @@ return {
       vim.keymap.set("n", "<space>.", builtin.find_files)
       vim.keymap.set("n", "<space>,", builtin.buffers)
       vim.keymap.set("n", "<space>/", builtin.live_grep)
-      vim.keymap.set("n", "<space>fg", builtin.git_files)
+      vim.keymap.set("n", "<space><space>", builtin.git_files)
       vim.keymap.set("n", "<space>fr", builtin.oldfiles)
       vim.keymap.set("n", "<space>hi", builtin.help_tags)
       vim.keymap.set("n", "<space>sf", builtin.current_buffer_fuzzy_find)
