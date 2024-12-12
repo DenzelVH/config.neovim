2abe9c4cca3ed5cd3571803e45e42e1d25379f5b
Denzel Wendy <d.wendy@verihubs.com>
2024-12-12 17:26:42 +0700

    add telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+    },
+    config = function()
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            theme = "ivy",
+          },
+
+          git_files = {
+            theme = "ivy",
+          },
+
+          help_tags = {},
+
+          live_grep = {
+            theme = "ivy",
+          },
+
+          current_buffer_fuzzy_find = {
+            theme = "ivy",
+          },
+
+          buffers = {
+            theme = "ivy",
+          },
+        },
+        extensions = {
+          fzf = {},
+        },
+      })
+
+      local builtin = require("telescope.builtin")
+
+      vim.keymap.set("n", "<space>.", builtin.find_files)
+      vim.keymap.set("n", "<space>,", builtin.buffers)
+      vim.keymap.set("n", "<space>/", builtin.live_grep)
+      vim.keymap.set("n", "<space>fg", builtin.git_files)
+      vim.keymap.set("n", "<space>fr", builtin.oldfiles)
+      vim.keymap.set("n", "<space>hi", builtin.help_tags)
+      vim.keymap.set("n", "<space>sf", builtin.current_buffer_fuzzy_find)
+    end,
+  },
+}
