495e7ea9d335017bd39188b94d519f9429d7f8d2
Denzel Wendy <d.wendy@verihubs.com>
2024-12-12 17:26:05 +0700

    enable mini.nvim features

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,10 +1,82 @@
 return {
   {
-    'echasnovski/mini.nvim',
+    "echasnovski/mini.nvim",
     enabled = true,
     config = function()
-      local statusline = require('mini.statusline')
-      statusline.setup { use_icons = true}
-    end
-  }
+      local statusline = require("mini.statusline")
+      statusline.setup({ use_icons = true })
+
+      local icons = require("mini.icons")
+      icons.setup({})
+
+      local bracketed = require("mini.bracketed")
+      bracketed.setup({
+        buffer = { suffix = "b", options = {} },
+        comment = { suffix = "c", options = {} },
+        conflict = { suffix = "x", options = {} },
+        diagnostic = { suffix = "d", options = {} },
+        file = { suffix = "f", options = {} },
+        indent = { suffix = "i", options = {} },
+        jump = { suffix = "j", options = {} },
+        location = { suffix = "l", options = {} },
+        oldfile = { suffix = "o", options = {} },
+        quickfix = { suffix = "q", options = {} },
+        treesitter = { suffix = "t", options = {} },
+        undo = { suffix = "u", options = {} },
+        window = { suffix = "w", options = {} },
+        yank = { suffix = "y", options = {} },
+      })
+
+      local miniclue = require("mini.clue")
+      miniclue.setup({
+        delay = 2000,
+        triggers = {
+          -- Leader triggers
+          { mode = "n", keys = "<Leader>" },
+          { mode = "x", keys = "<Leader>" },
+
+          -- Built-in completion
+          { mode = "i", keys = "<C-x>" },
+
+          -- `g` key
+          { mode = "n", keys = "g" },
+          { mode = "x", keys = "g" },
+
+          -- Marks
+          { mode = "n", keys = "'" },
+          { mode = "n", keys = "`" },
+          { mode = "x", keys = "'" },
+          { mode = "x", keys = "`" },
+
+          -- Registers
+          { mode = "n", keys = '"' },
+          { mode = "x", keys = '"' },
+          { mode = "i", keys = "<C-r>" },
+          { mode = "c", keys = "<C-r>" },
+
+          -- Window commands
+          { mode = "n", keys = "<C-w>" },
+
+          -- `z` key
+          { mode = "n", keys = "z" },
+          { mode = "x", keys = "z" },
+        },
+
+        clues = {
+          miniclue.gen_clues.builtin_completion(),
+          miniclue.gen_clues.g(),
+          miniclue.gen_clues.marks(),
+          miniclue.gen_clues.registers(),
+          miniclue.gen_clues.windows(),
+          miniclue.gen_clues.z(),
+        },
+      })
+
+      local pairs = require("mini.pairs")
+      pairs.setup({})
+
+      local indent = require("mini.indentscope")
+      indent.setup({})
+    end,
+  },
 }
