d35a3917476ea128813302edc5e75ede9947d4b8
Denzel Wendy <d.wendy@verihubs.com>
2024-12-12 13:04:32 +0700

    add treesitter and lsp

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 require("config.lazy")
 
-
 -- Vim opts
 vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,26 @@
+return {{
+  'neovim/nvim-lspconfig',
+  enabled = true,
+  dependencies = {
+    {
+      "folke/lazydev.nvim",
+      ft = "lua",
+      opts = {
+        library = {
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    }
+  },
+  opts = {
+    servers = {
+      lua_ls = {}
+    }
+  },
+  config = function(_, opts)
+    local lspconfig = require('lspconfig')
+    for server, config in pairs(opts.servers) do
+      lspconfig[server].setup(config)
+    end
+  end
+}}
