500bae2fc43dc40a293432630023fd6227111454
Denzel Wendy <d.wendy@verihubs.com>
2024-12-12 20:53:07 +0700

    formatter use recommended lazy.nvim setup

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,18 +1,31 @@
+-- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#lazy-loading-with-lazynvim
 return {
-  {
-    "stevearc/conform.nvim",
-    enabled = true,
-    config = function()
-      require("conform").setup({
-        formatters_by_ft = {
-          lua = { "stylua" },
-          ex = { "mix" },
-        },
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_format = "fallback",
-        },
-      })
-    end,
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
   },
+  ---@module "conform"
+  ---@type conform.setupOpts
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      elixir = { "mix" },
+    },
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+    format_on_save = { timeout_ms = 500 },
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
