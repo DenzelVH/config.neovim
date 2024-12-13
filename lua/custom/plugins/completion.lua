4d051979714552d9430e9b549b6b4950aca2b77c
Denzel Wendy <d.wendy@verihubs.com>
2024-12-13 10:12:55 +0700

    separate completion and LSP, also use mason

---
+++ lua/custom/plugins/completion.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "saghen/blink.cmp",
+    lazy = false,
+    version = "v0.*",
+    dependencies = "rafamadriz/friendly-snippets",
+    opts = {
+      keymap = { preset = "default" },
+      completion = { accept = { auto_brackets = { enabled = true } } },
+    },
+  },
+}
--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -3,6 +3,9 @@ return {
     "neovim/nvim-lspconfig",
     enabled = true,
     dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
       {
         "folke/lazydev.nvim",
         ft = "lua",
@@ -12,35 +15,33 @@ return {
           },
         },
       },
-
-      {
-        "saghen/blink.cmp",
-        lazy = false,
-        version = "v0.*",
-        dependencies = "rafamadriz/friendly-snippets",
-        opts = {
-          keymap = { preset = "default" },
-          completion = { accept = { auto_brackets = { enabled = true } } },
-        },
-      },
     },
-    -- Setup language server here
     opts = {
       servers = {
         lua_ls = {},
-        elixirls = {
-          cmd = { "elixir-ls" },
+        -- elixirls = {
+        --   cmd = { "elixir-ls" },
+        -- },
+        lexical = {
+          cmd = { "~/.local/share/nvim/mason/bin/lexical" },
         },
         yamlls = {},
         jsonls = {},
         terraformls = {},
         tailwindcss = {
+          init_options = {
+            userLanguages = {
+              elixir = "phoenix-heex",
+              eruby = "erb",
+              heex = "phoenix-heex",
+            },
+          },
           settings = {
             tailwindCSS = {
               includeLanguages = {
-                elixir = "html-eex",
-                eelixir = "html-eex",
-                heex = "html-eex",
+                elixir = "phoenix-heex",
+                eelixir = "phoenix-heex",
+                heex = "phoenix-heex",
               },
             },
           },
@@ -63,6 +64,25 @@ return {
         config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
         lspconfig[server].setup(config)
       end
+
+      require("mason").setup()
+      require("mason-lspconfig").setup()
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function()
+          local builtin = require("telescope.builtin")
+
+          vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+          vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
+          vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+          vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+          vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = 0 })
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = 0 })
+          vim.keymap.set("n", "<leader>wd", builtin.lsp_document_symbols, { buffer = 0 })
+        end,
+      })
     end,
   },
 }
