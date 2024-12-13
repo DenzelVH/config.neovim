33cb58ff1f4b663725d9eadb916e5df0c60fca9f
Denzel Wendy <d.wendy@verihubs.com>
2024-12-14 04:42:40 +0700

    and some good stuff

--- plugin/qol.lua
+++ plugin/qol.lua
@@ -5,3 +5,15 @@ opt.number = true
 opt.relativenumber = true
 opt.clipboard = "unnamedplus"
 opt.undofile = true
+opt.scrolloff = 8
+opt.sidescrolloff = 10
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line("'\"")
+    if line > 1 and line <= vim.fn.line("$") then
+      vim.cmd('normal! g`"')
+    end
+  end,
+})
