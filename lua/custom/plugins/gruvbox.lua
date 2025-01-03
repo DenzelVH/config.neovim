2b3870b0cb18c7c01fd3a5995e9acf476c5f506f
Denzel Wendy <d.wendy@verihubs.com>
2025-01-03 23:57:27 +0700

    change theme

--- lua/custom/plugins/gruvbox.lua
+++ lua/custom/plugins/gruvbox.lua
@@ -1,9 +1,2 @@
 return {
-  {
-    'ellisonleao/gruvbox.nvim',
-    enabled = true,
-    config = function()
-      vim.cmd('colorscheme gruvbox')
-    end
-  }
 }
---
+++ lua/custom/plugins/theme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "slugbyte/lackluster.nvim",
+    config = function()
+      vim.cmd('colorscheme lackluster-dark')
+    end
+  },
+  {
+    'ellisonleao/gruvbox.nvim',
+    enabled = true,
+    config = function()
+      -- vim.cmd('colorscheme gruvbox')
+    end
+  }
+}
